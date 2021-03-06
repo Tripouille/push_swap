a4d332b634860b84a6ff3f232f11651aad02f71c
Chinimala <astrid.allemandou@free.fr>
2021-03-06 13:29:57 +0100

    add ft_strcmp

--- includes/utils.h
+++ includes/utils.h
@@ -12,6 +12,7 @@ void	get_numbers(char **args, t_ilist *a);
 
 void	errorExit(void);
 int		ft_strlen(char const *s);
+int		ft_strcmp(char const *s1, char const *s2);
 bool	ft_isdigit(char c);
 
 #endif
