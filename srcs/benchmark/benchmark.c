46e1ee9604d7b554dbe39218513b182a470f2278
Tripouille <jmichel.gambard@gmail.com>
2021-03-08 12:47:45 +0100

    benchmark done

--- srcs/benchmark/benchmark.c
+++ srcs/benchmark/benchmark.c
@@ -1,6 +1,59 @@
 #include "benchmark.h"
 
+static void	benchmark_init(t_benchmark benchmark[])
+{
+	benchmark[0].algo = &franky_sort;
+	benchmark[1].algo = &franky_sort;
+	benchmark[2].algo = &franky_sort;
+	benchmark[3].algo = NULL;
+}
+
+static void benchmark_launch(t_instruction_infos instructions[], t_stacks *stacks,
+							t_benchmark benchmark[])
+{
+	int						i;
+	t_stacks				stacks_copy;
+
+	i = -1;
+	while (benchmark[++i].algo != NULL)
+	{
+		stacks_copy = stacks_clone(stacks);
+		benchmark[i].required_instructions = benchmark->algo(instructions, &stacks_copy);
+		stacks_destroy(&stacks_copy);
+	}
+}
+
+static void	benchmark_destroy(t_benchmark benchmark[])
+{
+	int						i;
+
+	i = -1;
+	while (benchmark[++i].algo != NULL)
+		slist_destroy(&benchmark[i].required_instructions);
+}
+
+static t_benchmark	*get_best_benchmark(t_benchmark benchmark[])
+{
+	return (benchmark);
+}
+
 void	print_best_algo(t_instruction_infos instructions[], t_stacks *stacks)
 {
-	
+	t_benchmark				benchmark[4];
+	t_benchmark				*best_benchmark;
+	t_slist_element			*instruction;
+
+	if (ilist_is_sort(&stacks->a, false))
+		return ;
+	benchmark_init(benchmark);
+	benchmark_launch(instructions, stacks, benchmark);
+	best_benchmark = get_best_benchmark(benchmark);
+	instruction = best_benchmark->required_instructions.head;
+	while (instruction != best_benchmark->required_instructions.tail)
+	{
+		printf("%s ", instruction->s);
+		instruction = instruction->next;
+	}
+	printf("%s\n", instruction->s);
+	benchmark_destroy(benchmark);
 }
--- srcs/utils/stacks.c
+++ srcs/utils/stacks.c
@@ -20,12 +20,18 @@ t_stacks	stacks_clone(t_stacks *src)
 	stacks_init(&clone);
 	element = src->a.head;
 	while (element != src->a.tail)
+	{
 		ilist_push(&clone.a, element->i);
+		element = element->next;
+	}
 	if (src->a.tail != NULL)
 		ilist_push(&clone.a, src->a.tail->i);
 	element = src->b.head;
 	while (element != src->b.tail)
+	{
 		ilist_push(&clone.b, element->i);
+		element = element->next;
+	}
 	if (src->b.tail != NULL)
 		ilist_push(&clone.b, src->b.tail->i);
 	return (clone);
