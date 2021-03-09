44d2cfc3cb673dc6b6dc97d33a1c56c58e3d8089
Tripouille <jmichel.gambard@gmail.com>
2021-03-09 20:08:42 +0100

    use finish_sorting_ordered_stacks in jm_sort

--- tester.rb
+++ tester.rb
@@ -13,7 +13,7 @@ end
 
 def doTests(numberLimit)
     results = []
-    100.times do
+    1000.times do
         numbers = getRandomNumber(numberLimit)
 		#puts numbers
         instructions = `#{'./push_swap ' + numbers}`
@@ -27,6 +27,6 @@ end
 print "TEST".center(30), "LOWEST".center(10), "BIGGEST".center(10), "AVERAGE".center(10); puts
 #print "Simple 5 random values".center(30); doTests(5)
 #print "Perso 8 random values".center(30); doTests(8)
-#print "Middle 100 random values".center(30); doTests(100)
-print "Advanced 500 random values".center(30); doTests(500)
+print "Middle 100 random values".center(30); doTests(100)
+#print "Advanced 500 random values".center(30); doTests(500)
 #print "Exterme 1000 random values".center(30); doTests(1000)
