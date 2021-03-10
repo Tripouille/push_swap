89666aff2f84b92d5ceaab08152d0004fecad72b
Astrid Allemandou <aalleman@z1r1p5.le-101.fr>
2021-03-10 16:17:21 +0100

    norm algorithm.c

--- srcs/algorithms/algorithms.c
+++ srcs/algorithms/algorithms.c
@@ -1,7 +1,7 @@
 #include "algorithms.h"
 
-void	stock_and_call(t_instruction_infos const instructions[], t_slist *required_instructions,
-						char const *name, t_stacks *stacks)
+void	stock_and_call(t_instruction_infos const instructions[],
+	t_slist *required_instructions, char const *name, t_stacks *stacks)
 {
 	slist_push(required_instructions, name);
 	instructions_call(instructions, name, stacks);
@@ -18,13 +18,11 @@ t_ilist_element	*get_biggest_below(t_ilist *ilist, int value)
 	element = ilist->head;
 	while (element != ilist->tail)
 	{
-		if (element->i < value &&
-		(result == NULL || element->i > result->i))
+		if (element->i < value && (result == NULL || element->i > result->i))
 			result = element;
 		element = element->next;
 	}
-	if (element->i < value &&
-	(result == NULL || element->i > result->i))
+	if (element->i < value && (result == NULL || element->i > result->i))
 		result = element;
 	return (result);
 }
@@ -46,10 +44,13 @@ void	finish_sorting_ordered_stacks(t_instruction_infos const instructions[],
 	}
 	else
 	{
-		a_steps = get_r_lengths(&stacks->a, ilist_get_smallest(&stacks->a), stacks->a.head);
-		b_steps = get_r_lengths(&stacks->b, ilist_get_smallest(&stacks->b), stacks->b.tail);
+		a_steps = get_r_lengths(&stacks->a, ilist_get_smallest(&stacks->a),
+				stacks->a.head);
+		b_steps = get_r_lengths(&stacks->b, ilist_get_smallest(&stacks->b),
+				stacks->b.tail);
 		best_rotates = get_best_rotates(&a_steps, &b_steps);
-		execute_rotates(instructions, stacks, required_instructions, &best_rotates);
+		execute_rotates(instructions, stacks, required_instructions,
+			&best_rotates);
 		while (!ilist_is_empty(&stacks->b))
 			stock_and_call(instructions, required_instructions, "pa", stacks);
 	}
