1aec4a5fab5956627234015fca8383f4e152c34e
Tripouille <jmichel.gambard@gmail.com>
2021-03-11 14:01:34 +0100

    change print_best_algo

--- srcs/benchmark/benchmark.c
+++ srcs/benchmark/benchmark.c
@@ -17,12 +17,16 @@ static void	benchmark_set(t_benchmark benchmark[], size_t pos,
 	slist_initialize(&benchmark[pos].required_instructions);
 }
 
-static void	benchmark_init(t_benchmark benchmark[])
+static void	benchmark_init(t_benchmark benchmark[], size_t list_size)
 {
-	benchmark_set(benchmark, 0, "selection_sort", &selection_sort);
-	benchmark_set(benchmark, 1, "jm_sort", &jm_sort);
-	benchmark_set(benchmark, 2, "simple_swap_sort", &simple_swap_sort);
-	benchmark_set(benchmark, 3, "", NULL);
+	size_t		i;
+
+	i = 0;
+	benchmark_set(benchmark, i++, "selection_sort", &selection_sort);
+	benchmark_set(benchmark, i++, "jm_sort", &jm_sort);
+	if (list_size < 100)
+		benchmark_set(benchmark, i++, "simple_swap_sort", &simple_swap_sort);
+	benchmark_set(benchmark, i, "", NULL);
 }
 
 static bool	benchmark_launch(t_instruction_infos const instructions[],
@@ -59,7 +63,7 @@ void	print_best_algo(t_instruction_infos const instructions[],
 
 	if (ilist_is_sort(&stacks->a, false))
 		return ;
-	benchmark_init(benchmark);
+	benchmark_init(benchmark, stacks->a.size);
 	if (!benchmark_launch(instructions, stacks, benchmark))
 		return ;
 	slist_show(&get_best_benchmark(benchmark)->required_instructions, '\n');
